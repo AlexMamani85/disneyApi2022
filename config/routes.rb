168196a964565a5dd72d735a93eed59c35b3835f
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-08-15 21:30:00 -0500

    CHORE: routes.rb for character controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  # get '/characters', to: 'character#index'
+  get '/characters/all', to: 'character#index_all'
+  get '/character/:id', to: 'character#show'
+  post '/character', to: 'character#create'
+  patch '/character/:id', to: 'character#update'
+  delete "/character/:id" => "character#destroy"
+  get "/characters", to: 'character#search'
+
 end
