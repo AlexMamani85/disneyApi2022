04b52dc5f76dce2d7b46fce3ed6166f6d79c0508
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-08-16 22:11:04 -0500

    CHORE: show characters for each movie

--- app/models/pelicula.rb
+++ app/models/pelicula.rb
@@ -1,4 +1,18 @@
 class Pelicula < ApplicationRecord
   belongs_to :genero
-  has_many :peliculas
+
+  def character
+    characters = Character.where(pelicula_id: id)
+    rpta = []
+    (0..characters.length - 1).each do |i|
+      rpta << { id: characters[i].id,
+                imagen: characters[i].imagen,
+                nombre: characters[i].nombre,
+                edad: characters[i].edad,
+                peso: characters[i].peso,
+                historia: characters[i].historia,
+                pelicula_id: characters[i].pelicula_id }
+    end
+    rpta
+  end
 end
